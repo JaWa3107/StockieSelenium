8a46b492dcdfcd85469a1dbbce3fc855b11eff61
Desktop-Jannik <jannik317@googlemail.com>
2022-01-08 18:47:43 +0100

    Test

---
+++ src/test/java/de/Stockie/TcCheckAvailable.java
@@ -0,0 +1,47 @@
+package de.Stockie;
+
+import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+public class TcCheckAvailable {
+
+WebDriver driver;
+	
+	@Before
+	public void initTests() throws MalformedURLException {
+		System.out.println("initialisiere Webdriver");
+		System.out.println("Start TcCheckAvailable");
+		
+		
+		URL linkHub = new URL("http://localhost:4444/wd/hub");
+		driver = new RemoteWebDriver(linkHub, DesiredCapabilities.firefox());
+		
+		//System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		//driver = new ChromeDriver();
+		driver.get("http://185.188.250.67/");	
+	}
+	
+	@After
+	public void afterTest() {
+		System.out.println("Test abgeschlossen. - Webdriver wird beendet");
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void testTitle() {
+		String expect = "Stockie";
+		assertEquals(expect, driver.getTitle());
+	}
+
+}
