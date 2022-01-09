5278750670f5c1d403c6cfe05d9932f9bd5af5b1
Desktop-Jannik <jannik317@googlemail.com>
2022-01-09 13:45:09 +0100

    Test

---
+++ src/test/java/de/Stockie/Page/StockieChartPage.java
@@ -0,0 +1,29 @@
+package de.Stockie.Page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class StockieChartPage {
+
+	private WebDriver driver;
+	
+	private By searchbarText = By.xpath("//input[@id='__BVID__7']");
+	private By searchbarButton = By.xpath("//div[@class='input-group-text']");
+	private By assetName = By.xpath("//div[@class='company']");
+	
+	public StockieChartPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void searchAsset(String asset) {
+		this.driver.findElement(searchbarText).sendKeys(asset);
+		this.driver.findElement(searchbarButton).click();
+	}
+	
+	public String getAssetName() {
+		return this.driver.findElement(assetName).getText();
+	}
+	
+}
--- src/test/java/de/Stockie/Selenium/TcCheckAvailable.java
+++ src/test/java/de/Stockie/Selenium/TcCheckAvailable.java
@@ -1,4 +1,4 @@
-package de.Stockie;
+package de.Stockie.Selenium;
 
 import static org.junit.Assert.*;
 
@@ -6,7 +6,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
