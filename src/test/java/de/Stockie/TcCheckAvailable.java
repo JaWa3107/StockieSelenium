87738f5300b941d502210236714b1e55017490d7
Desktop-Jannik <jannik317@googlemail.com>
2022-01-09 11:23:45 +0100

    wewerwerwer

--- src/test/java/de/Stockie/TcCheckAvailable.java
+++ src/test/java/de/Stockie/TcCheckAvailable.java
@@ -3,8 +3,12 @@ package de.Stockie;
 import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 
@@ -18,11 +22,11 @@ WebDriver driver;
 		System.out.println("initialisiere Webdriver");
 		System.out.println("Start TcCheckAvailable");
 		
-		//URL linkHub = new URL("http://localhost:4444/wd/hub");
-		//driver = new RemoteWebDriver(linkHub, DesiredCapabilities.firefox());
+		URL linkHub = new URL("http://localhost:4444/wd/hub");
+		driver = new RemoteWebDriver(linkHub, DesiredCapabilities.firefox());
 		
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		//driver = new ChromeDriver();
 		driver.get("http://185.188.250.67/");
 		
 		String expect = "Stockie";
